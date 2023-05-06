e8f249bf4bf4adde9324bf161e1b0b42f5063829
amirrezagh75 <mypersonal048@gmail.com>
2023-05-07 01:00:24 +0330

    removing log

--- db.py
+++ db.py
@@ -26,7 +26,6 @@ db_config = Settings()
 connectionString = f'mongodb://{db_config.DB_USER}:{db_config.DB_PASSWORD}@{db_config.DB_HOST}:{db_config.DB_PORT}/?authSource=admin' if db_config.DB_USER and db_config.DB_PASSWORD \
     else f'mongodb://{db_config.DB_HOST}:{db_config.DB_PORT}/'
 
-print(connectionString)
 maxSevSelDelay = 1 
 client = MongoClient(connectionString, serverSelectionTimeoutMS=maxSevSelDelay)
 db = client.nestTest
